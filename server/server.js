a2e48dd76fa2bf07ad2336cafb3bf5bbfa0a5f7c
Carter Walsh <Cwalsh616@gmail.com>
2021-07-15 13:26:54 -0500

    changed get pathway

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require("path");
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname));
 });
 
 const port = process.env.PORT || 3000
